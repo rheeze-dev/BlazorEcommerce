eddb1bfab987fafb1278622c48e6cf2872a315de
rheeze-dev <ggybzz2297@gmail.com>
2022-07-30 22:41:56 -0400

    AddedSearchBar

--- BlarozEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlarozEcommerce/Client/Services/ProductService/ProductService.cs
@@ -9,6 +9,7 @@
             _http = http;
         }
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "Loading products...";
 
         public event Action ProductsChanged;
 
@@ -26,6 +27,21 @@
             Products = result.Data;
 
             ProductsChanged.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchSuggestions/{searchText}");
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+            if (result != null && result.Data != null)
+                Products = result.Data;
+            if (Products.Count == 0) Message = "No products found.";
+            ProductsChanged?.Invoke();
         }
     }
 }
--- BlarozEcommerce/Server/Controllers/ProductController.cs
+++ BlarozEcommerce/Server/Controllers/ProductController.cs
@@ -34,5 +34,19 @@ namespace BlarozEcommerce.Server.Controllers
             var result = await _productService.GetProductsByCategory(categoryUrl);
             return Ok(result);
         }
+
+        [HttpGet("search/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> SearchProducts(string searchText)
+        {
+            var result = await _productService.SearchProducts(searchText);
+            return Ok(result);
+        }
+
+        [HttpGet("searchSuggestions/{searchText}")]
+        public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _productService.GetProductSearchSuggestions(searchText);
+            return Ok(result);
+        }
     }
 }
